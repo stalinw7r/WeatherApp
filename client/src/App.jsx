0adae3be4a016d7fd3b983271ca7858d296be06a
wesley <stalinw7@gmail.com>
2025-04-08 01:42:55 -0400

    added day night effect to main card, fixed minor value to make it more responsive

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,16 +2,12 @@ import { useState, useEffect } from "react";
 import Navbar from "./components/Navbar";
 import MainCard from "./components/MainCard";
 import "./App.css";
-import Alert from "./components/Alert";
-import axios from "axios";
-import ForecastSummary from "./components/ForecastSummary";
 import Footer from "./components/Footer";
 
 function App() {
   const [alert, setalert] = useState(false);
   const [value, setvalue] = useState("Kitchener, ON");
   const [display, setdisplay] = useState(false);
-  // const [currentdata, setcurrentdata] = useState();
 
   useEffect(() => {
     if (value !== undefined || value !== null) {
@@ -22,7 +18,7 @@ function App() {
   const onValueChange = (newvalue) => {
     setvalue(newvalue);
   };
-  // console.log(value);
+
   return (
     <>
       <div>
@@ -31,11 +27,10 @@ function App() {
           {display && (
             <div>
               <MainCard city={value} />
-
               <Footer />
             </div>
           )}
-          {!display && <div className="min-h-screen">thisi</div>}
+          {/* {!display && <div className="min-h-screen">thisi</div>} */}
         </div>
       </div>
     </>
